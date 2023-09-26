d6429e92f5c1bb6a2f05872560ede652cba4617f
seungsu <h2sorginal@gmail.com>
2023-09-26 16:49:00 +0900

    lab3 test1 ~ test5

---
+++ lab3/test2.cpp
@@ -0,0 +1,20 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    cout << "Socket ID:" << s << endl;
+    close(s);
+
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    cout << "Socket ID:" << s << endl;
+    close(s);
+
+    return 0;
+}
---
+++ lab3/test5.cpp
@@ -0,0 +1,44 @@
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    cout << "Socket ID:" << s << endl;
+
+    string buf = "";
+    while (1)
+    {
+        getline(cin, buf);
+        if(cin.eof())
+            break;
+
+        struct sockaddr_in sin;
+        memset(&sin, 0, sizeof(sin));
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(10001);
+        sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+        int numBytes = sendto(s, buf.c_str(), buf.length(), 0, (struct sockaddr *)&sin, sizeof(sin));
+        cout << "Sent: " << numBytes << endl;
+
+        char buf2[65536];
+        memset(&sin, 0, sizeof(sin));
+        socklen_t sin_size = sizeof(sin);
+        numBytes = recvfrom(s, buf2, sizeof(buf2), 0, (struct sockaddr *)&sin, &sin_size);
+        cout << "Recevied: " << numBytes << endl;
+        cout << "From " << inet_ntoa(sin.sin_addr) << endl;
+        cout << buf2 << endl;
+    }
+    
+    close(s);
+    return 0;
+}
