48d64770441c66aeb3150b918ec371bac0ec8ee6
GGeaBu <71253519+KKaeBu@users.noreply.github.com>
2023-12-02 22:26:55 +0900

    memo 서버 로컬 동작 구현

---
+++ memo_server/references/linux/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.1 (trial), 000000, 2023-11-27T19:07:16.073364
+from .pyarmor_runtime import __pyarmor__
---
+++ memo_server/references/macOS-apple-cpu/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.4 (trial), 000000, 2023-11-27T20:36:13.762678
+from .pyarmor_runtime import __pyarmor__
